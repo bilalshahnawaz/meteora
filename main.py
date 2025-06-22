870f2268c16aec0f5eddd94d95a2a27f4825f190
Bilal Shahnawaz <bilalshahnawaz@icloud.com>
2025-06-22 11:05:05 -0400

    Add player triangle and rotation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from database import connect_database, database_version
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
@@ -14,13 +16,17 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while (True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
-        pygame.display.flip()
         dt = clock.tick(60) / 1000 # Returns the amount of time since the last frame
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
